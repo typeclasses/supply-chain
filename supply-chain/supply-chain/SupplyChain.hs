9af37466232c3b8d8a40785a743642464f1da419
Chris Martin <ch.martin@gmail.com>
2023-01-11 16:04:07 -0700

    add module overview haddock

--- supply-chain/supply-chain/SupplyChain.hs
+++ supply-chain/supply-chain/SupplyChain.hs
@@ -1,6 +1,7 @@
 module SupplyChain
   (
 
+    {- * Modules -} {- $modules -}
     {- * Job type -} Job,
     {- * Making jobs -} order, perform,
     {- * Running jobs -} run, eval,
@@ -16,3 +17,15 @@ import SupplyChain.JobAndVendor (loop, loop', once, (>-), (>+))
 import SupplyChain.Referral (Referral (Referral))
 import SupplyChain.Unit (Unit (Unit))
 import SupplyChain.Vendor (Vendor (Vendor, handle), (>->))
+
+{- $modules
+
+* "SupplyChain.Alter" — Functions for modifying requests and actions
+* "SupplyChain.Effect" — An /effect/ is either /request/ or /perform/
+* "SupplyChain.Job" — A /job/ makes requests, performs actions, and returns
+* "SupplyChain.JobAndVendor" — /Job/ + /vendor/
+* "SupplyChain.Referral" — A /referral/ consists of a product and a new vendor
+* "SupplyChain.Unit" — /Unit/ is a simple interface with one request and a fixed response type
+* "SupplyChain.Vendor" — A /vendor/ responds to requests, makes requests, and performs actions
+
+-}
